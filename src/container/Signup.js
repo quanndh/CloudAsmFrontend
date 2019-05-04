72d81384be2d9698231bd81ba698545dbffa6792
quannguyennn <wantmorexx9x@gmail.com>
2019-05-04 16:27:35 +0700

    frontend cloud

--- src/container/Signup.js
+++ src/container/Signup.js
@@ -5,7 +5,7 @@ class SignUp extends Component {
     render() {
         return (
             <div class="login-form">
-                <form action="https://toyshop-server.herokuapp.com/api/user/"  method="post">
+                <form action="https://toyshop-server.herokuapp.com/api/users/"  method="post">
                     <h2 class="text-center">Sign Up</h2>   
                     <div class="form-group has-error">
                         <input type="text" class="form-control" name="account" placeholder="Username" required="required"/>
@@ -17,7 +17,7 @@ class SignUp extends Component {
                         <input type="text" class="form-control" name="name" placeholder="Username" required="required"/>
                     </div>      
                     <div class="form-group">
-                        <button type="submit" class="btn btn-primary btn-lg btn-block">Sign Up</button>
+                        <button type="submit" class="btn btn-primary btn-lg btn-block" onSubmit={this.handleOnSubmmit}>Sign Up</button>
                     </div>
                     Already have account? <Link to="/login"> Login</Link>
                 </form>
